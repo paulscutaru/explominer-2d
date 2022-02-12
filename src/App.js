59fdfe0428ae81ccb7739d6300b1daa4d94615aa
paulscutaru <61314329+paulscutaru@users.noreply.github.com>
2022-02-12 19:06:00 +0200

    Alpha version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,100 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap'
 import Grid from './components/Grid'
 
+let soundtrack = new Audio(process.env.PUBLIC_URL + "/sounds/soundtrack.mp3")
+
+const HEIGHT = 50, WIDTH = 10
+const EXPLOSION_SIZE = 2
+
+function generateRandomType(i, j) {
+  let random, type
+  random = Math.random()
+  if (random < 0.04)
+    type = "bonus"
+  else if ((random > 0.04 && random < 0.55) || (i < 3 && j < 3))
+    type = "empty"
+  else if (random > 0.55 && random < 0.65)
+    type = "trap"
+  else
+    type = "wall"
+  return type
+}
+
+function getImage(type) {
+  switch (type) {
+    case "empty":
+      return "cobblestone.png"
+    case "wall":
+      return "wall.png"
+    case "bonus":
+      return "bonus.png"
+    case "bomb":
+      return "bomb.png"
+    case "explosion":
+      return "explosion.png"
+    case "trap":
+      {
+        if (Math.random() > 0.5)
+          return "skull.png"
+        return "trap.png"
+      }
+
+  }
+}
+
+function GenerateGrid() {
+  let newGrid = []
+  let row
+  for (let i = 0; i < HEIGHT; i++) {
+    row = []
+    for (let j = 0; j < WIDTH; j++) {
+      let type = generateRandomType(i, j)
+      let img = getImage(type)
+      let info = { "img": img, "type": type, "x": i, "y": j, "last": type }
+      row.push(info)
+    }
+    newGrid.push(row);
+  }
+  return newGrid;
+}
+
+
+
 function App() {
+  const [gameStarted, setGameStarted] = useState(false)
+  const [grid, setGrid] = useState(GenerateGrid())
+
+  window.scroll({
+    top: 0,
+    left: 0,
+  });
+
+  function startGame() {
+    setGameStarted(true)
+    soundtrack.play()
+
+  }
+
   return (
     <Container className='game'>
-      <Grid />
+      {!gameStarted &&
+        <Container className="game-won-modal start">
+          <img src={process.env.PUBLIC_URL + "/images/skull.png"} />
+          <img src={process.env.PUBLIC_URL + "/images/player_bomb.png"} />
+          <img src={process.env.PUBLIC_URL + "/images/skull.png"} />
+          <h2>💣Explominer 2D💣</h2>
+          <h3>How to play</h3>
+          <p>• Collect all torches.</p> 
+          <p>• Avoid skulls or poison.</p>
+          <p>• Use bombs wisely.</p>
+          <button className="button btn-restart" onMouseDown={() => startGame()}>Start</button>
+        </Container>
+      }
+      {gameStarted &&
+        <Grid grid={grid} HEIGHT={HEIGHT} WIDTH={WIDTH}
+          EXPLOSION_SIZE={EXPLOSION_SIZE} />}
     </Container>
   );
 }
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,26 +1,11 @@
-import { useState, useReducer } from "react";
+import { useState, useReducer, useRef } from "react";
 import { Container, Row, Col } from 'react-bootstrap'
 import Cell from './Cell'
 
-const HEIGHT = 60, WIDTH = 10
-const EXPLOSION_SIZE = 2
-var totalBonuses = 0
-
-function generateRandomType(i, j) {
-    let random, type
-    random = Math.random()
-    if (random < 0.02) {
-        type = "bonus"
-        totalBonuses += 1
-    }
-    else if ((random > 0.02 && random < 0.65) || (i < 3 && j < 3))
-        type = "empty"
-    else if (random > 0.65 && random < 0.70)
-        type = "trap"
-    else
-        type = "wall"
-    return type
-}
+let explodeSound = new Audio(process.env.PUBLIC_URL + "/sounds/explode.mp3")
+let bonusSound = new Audio(process.env.PUBLIC_URL + "/sounds/bonus.mp3")
+let endSound = new Audio(process.env.PUBLIC_URL + "/sounds/end.mp3")
+let winSound = new Audio(process.env.PUBLIC_URL + "/sounds/win.mp3")
 
 function getImage(type) {
     switch (type) {
@@ -39,31 +24,24 @@ function getImage(type) {
     }
 }
 
-function GenerateGrid() {
-    let newGrid = []
-    let row
-    for (let i = 0; i < HEIGHT; i++) {
-        row = []
-        for (let j = 0; j < WIDTH; j++) {
-            let type = generateRandomType(i, j)
-            let img = getImage(type)
-            let info = { "img": img, "type": type, "x": i, "y": j }
-            row.push(info)
-        }
-        newGrid.push(row);
-    }
-    return newGrid;
-}
+function Grid(props) {
+    let grid = props.grid
+    let HEIGHT = props.HEIGHT
+    let WIDTH = props.WIDTH
+    let EXPLOSION_SIZE = props.EXPLOSION_SIZE
+    var totalBonuses = 0
+    for (let i = 0; i < HEIGHT; i++)
+        for (let j = 0; j < WIDTH; j++)
+            if (grid[i][j].last === "bonus")
+                totalBonuses += 1
 
-
-function Grid() {
-    const [grid, setGrid] = useState(GenerateGrid())
-    const [currentCell, setCurrentCell] = useState(grid[0][0])
+    const [currentCell, setCurrentCell] = useState(grid[1][1])
     const [buttonBombEnabled, setButtonBombEnabled] = useState(true)
-    const [gameStarted, setGameStarted] = useState(false)
-    const [gameOver, setGameOver] = useState(false)
+    const gameOver = useRef(false)
+    const gameWon = useRef(false)
+    const destroyedTorch = useRef(false)
     const [bonus, setBonus] = useState(0)
-    const [bombs, setBombs] = useState(totalBonuses + 5)
+    const [bombs, setBombs] = useState(Math.round(totalBonuses * 0.7))
     const forceUpdate = useReducer(() => ({}))[1]
 
     function canMove(x, y) {
@@ -72,26 +50,57 @@ function Grid() {
         return true
     }
 
+    function checkExplosion(x, y) {
+        if (grid[x][y].type === "bonus") {
+            gameOver.current = true
+            playSound("end")
+            destroyedTorch.current = true
+        }
+    }
+
+    function playSound(sound) {
+        switch (sound) {
+            case "explode":
+                explodeSound.play()
+                break
+            case "bonus":
+                bonusSound.play()
+                break
+            case "win":
+                winSound.play()
+                break
+            case "end":
+                endSound.play()
+                break
+        }
+    }
+
     function explodeBomb() {
         let x = currentCell.x, y = currentCell.y, explosionImage = getImage("explosion"), emptyImage = getImage("empty")
+        playSound("explode")
 
+        checkExplosion(x, y)
         grid[x][y].type = "explosion"
         grid[x][y].img = explosionImage
 
         for (let i = 1; i <= EXPLOSION_SIZE; i++) {
             if ((x + i) < HEIGHT) {
+                checkExplosion(x + i, y)
                 grid[x + i][y].type = "explosion"
                 grid[x + i][y].img = explosionImage
             }
             if ((x - i) >= 0) {
+                checkExplosion(x - i, y)
                 grid[x - i][y].type = "explosion"
                 grid[x - i][y].img = explosionImage
             }
             if ((y - i) >= 0) {
+                checkExplosion(x, y - i)
                 grid[x][y - i].type = "explosion"
                 grid[x][y - i].img = explosionImage
             }
             if ((y + i) < WIDTH) {
+                checkExplosion(x, y + i)
                 grid[x][y + i].type = "explosion"
                 grid[x][y + i].img = explosionImage
             }
@@ -134,7 +143,7 @@ function Grid() {
             setTimeout(() => {
                 explodeBomb()
                 setButtonBombEnabled(true)
-            }, 1300);
+            }, 700);
         }
     }
 
@@ -161,35 +170,49 @@ function Grid() {
 
         if (canMove(newX, newY)) {
             if (grid[newX][newY].type === "trap") {
-                setGameOver(true)
+                playSound("end")
+                gameOver.current = true
             }
             if (grid[newX][newY].type === "bonus") {
+                playSound("bonus")
+
                 grid[newX][newY].type = "empty"
                 grid[newX][newY].img = getImage("empty")
-                setBonus(bonus + 1)
+
+                setBonus(bonus => bonus + 1)
+
+                if ((bonus + 1) === totalBonuses) {
+                    gameWon.current = true
+                }
+
             }
             setCurrentCell(grid[newX][newY])
         }
     };
 
     function restartGame() {
-        setGrid(GenerateGrid())
-        setCurrentCell(grid[0][0])
-        setGameOver(false)
-        setBonus(0)
-        setBombs(totalBonuses + 5)
-        console.log("Game restarted")
+        gameOver.current = false
+        window.location.reload();
+
     }
 
     return (
         <Container>
+            {gameOver.current &&
+                <Container className="game-over-modal">
+                    <h2>Game over</h2>
+                    {destroyedTorch.current && <h3>You destroyed a torch!</h3>}
+                    {!destroyedTorch.current && <h3>You got killed!</h3>}
+                    <button className="button btn-restart" onMouseDown={() => restartGame()}>Go back</button>
+                </Container>
+            }
+            {gameWon.current &&
+                <Container className="game-won-modal">
+                    <h2>💣Game won!💣</h2>
+                    <button className="button btn-restart" onMouseDown={() => restartGame()}>Go back</button>
+                </Container>
+            }
             <Container className="grid">
-                {gameOver &&
-                    <Container className="game-over-modal">
-                        <h2>You died from poisoning!</h2>
-                        <button className="button btn-restart" onMouseDown={() => restartGame()}>Restart</button>
-                    </Container>
-                }
                 <Container className="score-container">
                     <h4>Torches: {bonus} / {totalBonuses}</h4>
                     <h4>💣{bombs}</h4>
@@ -202,7 +225,7 @@ function Grid() {
                     </Row>
                 ))}
             </Container>
-            {!gameOver &&
+            {!gameOver.current &&
                 <Container className="controls" >
                     <Row>
                         <Col>
